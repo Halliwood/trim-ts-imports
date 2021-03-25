591a12ab14f20251c26660873f9fa337514a06e4
Halliwood <linxiaojia8802@126.com>
2021-03-25 20:17:03 +0800

    fix error

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,5 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var cli_1 = __importDefault(require("cli"));
 var ImportTrimer_1 = require("./ImportTrimer");
-var args = cli_1.default.parse({
-    file: ['f', 'The file to process', 'file', '']
-});
-if (!args.file) {
-    cli_1.default.error('-f or --file is requred.');
-    cli_1.default.exit(1);
-}
 var trimer = new ImportTrimer_1.ImportTrimer();
-trimer.trimFile(args.file);
 exports.default = trimer;
